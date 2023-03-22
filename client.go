6740e15205edda6ef744c8a4f6559d81690dbd3e
Nishma056 <nishmae5@gmail.com>
2023-03-22 14:05:58 +0530

    first commit

---
+++ client.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/Nishma056/myprograms/myservice"
+
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewMyServiceClient(conn)
+
+	stream, err := c.BidirectionalStream(context.Background())
+	if err != nil {
+		log.Fatalf("failed to call BidirectionalStream: %v", err)
+	}
+
+	defer stream.CloseSend()
+
+	go func() {
+		for _, req := range []*pb.Request{
+			{Value: "Alice"},
+			{Value: "Bob"},
+			{Value: "Charlie"},
+			{Value: "Dave"},
+		} {
+			if err := stream.Send(req); err != nil {
+				log.Fatalf("failed to send request: %v", err)
+			}
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to receive response: %v", err)
+		}
+
+		log.Printf("received response: %v", resp.Value)
+	}
+}
---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/Nishma056/myprograms/myservice"
+
+	"google.golang.org/grpc"
+)
+
+type myServiceServer struct{}
+
+func (s *myServiceServer) BidirectionalStream(stream pb.MyService_BidirectionalStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		resp := pb.Response{
+			Value: "Hello " + req.Value,
+		}
+
+		if err := stream.Send(&resp); err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterMyServiceServer(s, &myServiceServer{})
+
+	fmt.Println("Server listening on port 50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
